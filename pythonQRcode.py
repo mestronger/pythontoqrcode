b86e624386a620c32b44fd48003c234624b2069e
I must be strong <164299950+mestronger@users.noreply.github.com>
2024-03-21 21:33:29 +0700

    Update pythonQRcode.py

--- pythonQRcode.py
+++ pythonQRcode.py
@@ -1,5 +1,19 @@
-import QRcode
+import qrcode
 
-img = QRcode.make('https://wa.me/6281378893790')
-img.save('myQRcod.png')
-img.show()
+# Tekst atau data yang ingin Anda konversi menjadi QR code
+data = "https://wa.me/6281378893790"
+
+# Membuat objek QRCode
+qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+
+# Menambahkan data ke QRCode
+qr.add_data(data)
+qr.make(fit=True)
+
+# Membuat gambar QRCode
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Menyimpan gambar QRCode sebagai file
+img.save("qrcode.png")
+
+print("QR code berhasil dibuat.")
